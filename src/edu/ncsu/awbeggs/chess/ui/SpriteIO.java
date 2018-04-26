9ab8e6d118a920b25841b882644d5b6fc110b00b
Aidan Beggs <awbeggs@ncsu.edu>
2018-04-26 16:32:39 -0400

    updated file path searching for sprite files

--- src/edu/ncsu/awbeggs/chess/ui/SpriteIO.java
+++ src/edu/ncsu/awbeggs/chess/ui/SpriteIO.java
@@ -3,6 +3,7 @@ package edu.ncsu.awbeggs.chess.ui;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -11,12 +12,12 @@ import edu.ncsu.awbeggs.chess.model.piece.PieceColor;
 public class SpriteIO {
 	
 	public static BufferedImage readSpriteFromPath(String name, PieceColor color) {
-		String path = "sprites/" + (color == PieceColor.WHITE ? "white" : "black") + "/" + name + ".png";
-		
+		URL urlPath = SpriteIO.class.getResource("/" + (color == PieceColor.WHITE ? "white" : "black") + "/" + name + ".png");
+		System.out.println(urlPath);
 		BufferedImage in;
 		
 		try {
-			in = ImageIO.read(new File(path));
+			in = ImageIO.read(urlPath);
 		} catch (IOException e) {
 			throw new IllegalArgumentException();
 		}
