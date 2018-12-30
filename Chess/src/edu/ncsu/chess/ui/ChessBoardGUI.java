ee058f6cae95390f87203803debc7a64c02ebebd
Aidan Beggs <awbeggs@ncsu.edu>
2018-12-30 01:10:49 -0500

    removed some unused imports

--- Chess/src/edu/ncsu/chess/ui/ChessBoardGUI.java
+++ Chess/src/edu/ncsu/chess/ui/ChessBoardGUI.java
@@ -6,8 +6,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
 import java.util.List;
 
 import javax.swing.JFrame;
